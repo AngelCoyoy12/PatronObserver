0394c784a5e749a67b6335b6122852f86da26a5b
Angel Coyoy <furioso7s12@gmail.com>
2024-07-01 11:40:43 -0600

    First commit

---
+++ src/ControlTemperatura/MainTemperature.java
@@ -0,0 +1,21 @@
+package ControlTemperatura;
+
+public class MainTemperature {
+    public static void main(String[] args) {
+        // Creamos una instancia de WeatherStation
+        WeatherStation weatherStation = new WeatherStation();
+
+        // Creamos instancias de los observadores
+        TemperatureDisplay temperatureDisplay = new TemperatureDisplay();
+        WeatherWarning weatherWarning = new WeatherWarning();
+
+        // Agregamos los observadores a la estación meteorológica
+        weatherStation.addObserver(temperatureDisplay);
+        weatherStation.addObserver(weatherWarning);
+
+        // Simulamos cambios en la temperatura
+        weatherStation.setTemperature(25);
+        weatherStation.setTemperature(30);
+    }
+}
+
---
+++ src/ControlTemperatura/TemperatureDisplay.java
@@ -0,0 +1,8 @@
+package ControlTemperatura;
+
+class TemperatureDisplay implements Observer {
+    @Override
+    public void update(int temperature) {
+        System.out.println("Temperature Display: La temperatura actual es: " + temperature);
+    }
+}
